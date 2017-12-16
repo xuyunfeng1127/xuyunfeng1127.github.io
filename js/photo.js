c69a163bdbc4d46d30152d77aa9d833587475610
xuyunfeng1127 <2485447358@qq.com>
2017-12-16 18:37:51 +0800

    Site updated: 2017-12-16 18:37:50

--- js/photo.js
+++ js/photo.js
@@ -18,8 +18,8 @@ define([], function () {
                 var html, li = "";
                 for (var i = begin; i < end && i < data.length; i++) {
                     li += '<li><div class="img-box">' +
-                        '<a class="img-bg" rel="example_group" href="https://github.com/xuyunfeng1127/blog/blob/master/photos/' + data[i] + '?raw=true"></a>' +
-                        '<img lazy-src="https://github.com/xuyunfeng1127/blog/blob/master/photos/' + data[i] + '?raw=true" />' +
+                        '<a class="img-bg" rel="example_group" href="https://github.com/xuyunfeng1127/master/photos/' + data[i] + '?raw=true"></a>' +
+                        '<img lazy-src="https://github.com/xuyunfeng1127/master/photos/' + data[i] + '?raw=true" />' +
                         '</li>';
                 }
 
